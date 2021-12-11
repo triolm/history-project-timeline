578ff327bacc9c7f6c394149614c851655920ebb
Marisa Triola <marisatriola@gmail.com>
2021-12-10 23:08:52 -0500

    user can now input data

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-const path = require('path')
+const path = require('path');
 
 app.set("view engine", "ejs")
-app.set('path', __dirname + "views");
+app.set('path', path.join(__dirname + "views"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get("/", (req, res) => {
-    res.render("index", { sheet })
+    res.render("form")
+})
+
+app.post("/rendered", (req, res) => {
+    data = parse(req.body.txt);
+    res.render("index", { sheet: data })
 })
 
 app.listen(3000, (req, res) => {
@@ -15,100 +23,16 @@ app.listen(3000, (req, res) => {
 })
 
 
-sheet = [
-    {
-        "year": 1900,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1901,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1902,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1903,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1904,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1905,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1906,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1907,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1908,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1909,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1910,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1911,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1912,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1913,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1914,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1915,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1916,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1917,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
-    },
-    {
-        "year": 1918,
-        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente architecto eveniet rerum commodi, a omnis atque non, laboriosam nam aperiam iste maiores similique mollitia facere officia amet voluptatem. Ut, dolor?",
-        "url": "www.google.com"
+parse = (str) => {
+    let json = []
+    let arr = str.split("\n")
+    for (i of arr) {
+        el = i.split('\t')
+        j = {}
+        j.year = el[0]
+        j.text = el[1]
+        j.url = el[2]
+        json.push(j)
     }
-]
+    return json;
+}
